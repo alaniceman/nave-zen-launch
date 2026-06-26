aa411ac5bc04e414e205aa8aee13d905c528d6e9
alaniceman <alan.earle@gmail.com>
2026-06-26 19:12:08 +0000

    Build: (6207fea) Activó etiquetas gtag
    X-Lovable-Edit-ID: edt-6f8dee77-192c-40a7-a469-890e4a9b873d
    Co-authored-by: alaniceman <224975431+alaniceman@users.noreply.github.com>

---
+++ assets/CheckoutRedirectButton-DN5lYIMV.js
@@ -0,0 +1,1 @@
+import{j as e,B as s}from"./index-DRRNe4Q1.js";const n=({children:t,url:o,className:a="",plan:r})=>e.jsx(s,{"data-checkout-url":o,"data-plan":r,className:a,children:t});export{n as C};
