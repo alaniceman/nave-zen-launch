0d260f496b6c22b0b731ff05fe9b253c37b23bde
alaniceman <alan.earle@gmail.com>
2026-07-24 15:49:03 +0000

    Build: (8519d63) Actualizó meta tags de Home
    X-Lovable-Edit-ID: edt-bada1ebe-3a2b-41a6-a342-fe889113c818
    Co-authored-by: alaniceman <224975431+alaniceman@users.noreply.github.com>

--- assets/CheckoutRedirectButton-RmtoXbJV.js
+++
@@ -1,1 +0,0 @@
-import{j as e,B as s}from"./index-tx5y1KmQ.js";const n=({children:t,url:o,className:a="",plan:r})=>e.jsx(s,{"data-checkout-url":o,"data-plan":r,className:a,children:t});export{n as C};
